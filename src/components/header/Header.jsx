afe7d27292adb9f743a5d0b520f0514b01e6cd13
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-11-08 17:08:30 +0200

    feat:create Header componets

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import quizLogo from "../../assets/quiz-logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={quizLogo} alt="logo" />
+      <h1>React Quiz</h1>
+    </header>
+  );
+};
+
+export default Header;
