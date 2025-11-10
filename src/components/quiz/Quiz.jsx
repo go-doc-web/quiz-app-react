1f54ee82a1769876981ab855066603452d2ff47e
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-11-10 10:30:20 +0200

    feat: logic shuffled answers and render quiz completed

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
-
+import { useState } from "react";
 import QUESTIONS from "../../data/questionsUkr";
-
-const items = QUESTIONS;
+import quizComplited from "../../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -15,12 +13,27 @@ const Quiz = () => {
     });
   };
 
+  const isQuizCompleted = activeQuestionIndex === QUESTIONS.length;
+
+  if (isQuizCompleted) {
+    return (
+      <>
+        <div id="summary">
+          <img src={quizComplited} alt="Quiz Complited" />
+          <h2>Quiz Completed</h2>
+        </div>
+      </>
+    );
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
-        <h2>{items[activeQuestionIndex].text}</h2>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {items[activeQuestionIndex].answers.map((answer) => {
+          {shuffledAnswers.map((answer) => {
             return (
               <li key={answer} className="answer">
                 <button onClick={() => handleAnswerButton(answer)}>
